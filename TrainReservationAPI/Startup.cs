ef3195257053e55f93fea46ff1ef196bde2ef122
Afran Mirad <afranmirad@gmail.com>
2021-08-08 13:04:26 +0300

    update

--- TrainReservationAPI/Startup.cs
+++ TrainReservationAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace TrainReservationAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
